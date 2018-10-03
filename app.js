8509a36fe30ce5cea86fdf3f2013d8f5759ac9da
Leandro Vernengo <Leandro.Vernengo@marconi.loc>
2018-10-03 14:42:51 +0200

    Restored app.js

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ var server = http.createServer(function (request, response)
          response.write('hello client!');    
          response.end();
     });
+server.listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
