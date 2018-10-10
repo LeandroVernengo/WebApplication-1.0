7e3e47ac4974d4d9ab1fd5c39b98b82cfef301d2
Leandro Vernengo <Leandro.Vernengo@marconi.loc>
2018-10-10 14:51:04 +0200

    Modified app.js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require("express");
-var path = require("path");
-var fs = require("fs");
+var http = require("http");
+var util= require("util");
+
 var app = express();
 
-app.use(function(req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
+app.use(function(request, response, next) {
+    //console.log("In comes a " + request.method + " to " + request.url);
+    //console.log(util.inspect(request, {showHidden: false, depth: null}));
+   // console.log(util.inspect(request.query));
+      var query = request.query;
+    
+      query.p=query.p+"test";
+      console.log(query.p);
+      console.log(query.p2);
+      
+      next();
+});
+
+app.use(function(request, response, next) {
+    console.log("Secondo mid " + request.method + " to " + request.url);
     next();
 });
 
-app.listen(3000, function() {
-    console.log("App started on port 3000");
+app.use(function(request, response) {
+    var query = request.query;
+    //response.writeHead(200, { "Content-Type": "text/plain" });
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("Hello, world! <br>"+query.p);
 });
+
+http.createServer(app).listen(3000);
